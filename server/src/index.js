04c49b24137fdc2005b61b3ab5fce92bc8fb25d3
GreenTeethSkull <angelitorios366@gmail.com>
2023-07-21 00:29:46 -0500

    promises integration complete

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,35 @@
 const http = require('http');
-const data = require('./utils/data');
+// const data = require('./utils/data');
 const { stringify } = require('querystring');
+const { getCharById } = require('./controllers/getCharById');
 
 const PORT = 3001;
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    if (req.url.includes('/rickandmorty/character/')) {
+    // if (req.url.includes('/rickandmorty/character/')) {
 
-        let id = req.url.split('/').at(-1);
-        let car = data.find(obj => obj.id === Number(id));
+    //     let id = req.url.split('/').at(-1);
+    //     let car = data.find(obj => obj.id === Number(id));
 
         
-        if (car) {
-            res.writeHead(200, { "Content-Type": "application/json" });
-            return res.end(JSON.stringify(car));
-        } else {
-            res.writeHead(404, { "Content-Type": "text/plain" });
-            return res.end('Id not Found');
-        }
+    //     if (car) {
+    //         res.writeHead(200, { "Content-Type": "application/json" });
+    //         return res.end(JSON.stringify(car));
+    //     } else {
+    //         res.writeHead(404, { "Content-Type": "text/plain" });
+    //         return res.end('Id not Found');
+    //     }
 
-    } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        return res.end('Route not found');
+    // } else {
+    //     res.writeHead(404, { "Content-Type": "text/plain" });
+    //     return res.end('Route not found');
+    // }
+
+    if (req.url.includes('/rickandmorty/character/')) {
+        let id = req.url.split('/').at(-1);
+        getCharById(res,id);
     }
 
 }).listen(PORT, 'localhost');
